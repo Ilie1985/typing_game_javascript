6b38e64b2df62500c7b0cceb18a0e11e9120bdc3
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-05 19:49:21 +0100

    add word to dom function

--- script.js
+++ script.js
@@ -7,3 +7,55 @@ const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
+
+//List of words for game
+
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+//Init word
+
+let randomWord;
+
+//init score
+
+let score = 0;
+
+//init time
+
+let time = 10;
+
+//create function to get random word
+
+const getRandomWord = () => {
+  return words[Math.floor(Math.random() * words.length)];
+};
+
+console.log(getRandomWord());
+
+//add word to DOM
+
+const addWordToDom = () => {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+};
+addWordToDom();
